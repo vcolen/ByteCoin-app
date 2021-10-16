fa1a6b612594cc86cc9bd03341126f80a6c7de9a
VIctor <colen.victor@icloud.com>
2021-10-16 00:34:25 -0300

    added CoinData, added functions to CoinManager

---
+++ ByteCoin/Model/CoinData.swift
@@ -0,0 +1,14 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by Victor Colen on 16/10/21.
+//  Copyright © 2021 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData: Codable{
+    let rate: Double
+}
+    
--- ByteCoin/Model/CoinManager.swift
+++ ByteCoin/Model/CoinManager.swift
@@ -10,10 +10,45 @@ import Foundation
 
 struct CoinManager {
     
-    let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
-    let apiKey = "YOUR_API_KEY_HERE"
+    let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC/"
+    let apiKey = ProcessInfo.processInfo.environment["coinApiKey"]
     
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+    
+    func getCoinPrice(for currency: String) {
+        let url = baseURL + "\(currency)?apikey=\(apiKey!)"
+        performRequest(with: url)
+    }
+    
+    func parseJSON(_ data: Data) -> Double? {
+        let decoder = JSONDecoder()
+        do {
+            let decodedData = try decoder.decode(CoinData.self, from: data)
+            let lastPrice = decodedData.rate
+            
+            return lastPrice
+        } catch {
+            print(error)
+            return nil
+        }
+    }
+    
+    func performRequest(with urlString: String) {
+        if let url = URL(string: urlString){
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url) { data, response, error in
+                if error != nil {
+                    print(error!)
+                }
+                if let safeData = data {
+                    let price = parseJSON(safeData)
+                    let formattedPrice = String(format: "%.2f", price!)
+                    print(formattedPrice)
+                }
+            }
+            task.resume()
+        }
+    }
 
     
 }
